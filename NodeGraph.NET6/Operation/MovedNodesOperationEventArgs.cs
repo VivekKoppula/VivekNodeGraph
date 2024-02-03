6483a71d63c6c29f6c47bb48c3902c568d1345e9
Vivekanand Swamy Koppula <Vivekanand.Swamy@gmail.com>
2024-02-03 19:12:02 +0530

    Initial commit after copying

---
+++ NodeGraph.NET6/Operation/MovedNodesOperationEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NodeGraph.NET6.Operation
+{
+    public class NodesMovedOperationEventArgs : EventArgs
+    {
+        public Guid[] NodeGuids { get; } = null;
+
+        public NodesMovedOperationEventArgs(Guid[] nodeGuids)
+        {
+            NodeGuids = nodeGuids;
+        }
+    }
+}
---
+++ NodeGraph/Operation/BeginMoveNodesOperationEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NodeGraph.Operation
+{
+    public class BeginMoveNodesOperationEventArgs : EventArgs
+    {
+        public Guid[] NodeGuids { get; } = null;
+
+        public BeginMoveNodesOperationEventArgs(Guid[] nodeGuids)
+        {
+            NodeGuids = nodeGuids;
+        }
+    }
+}
